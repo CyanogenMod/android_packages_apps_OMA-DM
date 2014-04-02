305faca2b66a07a037f1ee9a741254f025b4c05b
Ben Murdoch <benm@google.com>
2014-04-02 09:54:35 +0000

    Revert "Initial commit of new OMA DM client."
    This reverts commit 9305001cc2c1ee5fe5830e2554ed8d4f269ebe0e.
    
    Breaking checkbuild with the following failure:
    make: *** No rule to make target `packages/apps/OMA-DM/plugins/CONNMO/src/com/android/sdm/plugins/connmo/ConnmoTargetQC.java', needed by `out/target/common/obj/APPS/ConnMO_intermediates/classes-full-debug.jar'.  Stop.
    make: *** Waiting for unfinished jobs....
    
    Change-Id: I5116d2d64c9b1253443114ddd5ff095c0c74e808

--- engine/dmlib/tool-src/db_wizard_tool/com/mot/dm/dbtool/ValTab.java
+++
@@ -1,6 +0,0 @@
-package com.mot.dm.dbtool;
-
-public class ValTab {
-  public Byte encVal = null;
-  public int table = 0x00;
-}
--- engine/dmlib/tool-src/dmt_validate/com/mot/treetest/Content.java
+++
@@ -1,6 +0,0 @@
-package com.mot.treetest;
-
-public class Content {
-  int lines = 0;
-  String tests = "";
-}
